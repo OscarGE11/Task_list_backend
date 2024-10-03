d8836eedd2e666d3c85f1f76770d2fc7141b3cf2
OscarGE11 <geoscar1100@gmail.com>
2024-10-03 02:13:33 +0200

    created db configuration

---
+++ config/db.py
@@ -0,0 +1,8 @@
+from sqlalchemy import create_engine, MetaData
+
+engine = create_engine("mysql+pymysql://root:password@localhost:3306/task_list")
+meta = MetaData()
+
+
+conn = engine.connect()
+
---
+++ models/task.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Table, Column, Integer, String, DateTime, ForeignKey
+from config.db import meta, engine
+
+
+tasks = Table("tasks", meta,
+              Column("id", Integer, primary_key=True),
+              Column("title", String(255), nullable=False),
+              Column("description", String(255), nullable=False),
+              Column("created_at", DateTime, nullable=False),
+              Column("updated_at", DateTime, nullable=False))
+
+meta.create_all(engine)
