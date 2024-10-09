ed30f3b9d873f11bc6b33f064fde67a96d1e74ea
OscarGE11 <geoscar1100@gmail.com>
2024-10-09 13:59:54 +0200

    Updated db password

--- config/db.py
+++ config/db.py
@@ -2,7 +2,8 @@ from sqlalchemy import create_engine, MetaData
 from sqlalchemy.exc import SQLAlchemyError
 
 # Conexión con la base de datos MySQL en XAMPP
-DATABASE_URL = "mysql+pymysql://root:password@localhost:3306/task_list"
+DATABASE_URL = "mysql+pymysql://root:@localhost:3306/task_list"
+
 
 # Crear el motor de la base de datos
 try:
